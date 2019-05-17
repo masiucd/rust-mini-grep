d436d04cf707a8854830a8ee2b1a2185f8fa1fd3
marcell <ciszekmarcell@gmail.com>
2019-05-17 18:02:34 +0200

    react lottery

--- react_sandbox/src/components/lottery/BallGame.js
+++ react_sandbox/src/components/lottery/BallGame.js
@@ -14,28 +14,46 @@ export default class BallGame extends Component {
     nums: Array.from({ length: this.props.maxBalls }),
   };
 
-  generate = () => {};
+  generate = () => {
+    this.setState(prevState => ({
+      nums: prevState.nums.map(
+        n => Math.floor(Math.random() * this.props.maxNum) + 1
+      ),
+    }));
+  };
 
-  handleClick = () => {};
+  handleClick = () => {
+    this.generate();
+  };
 
   render() {
     return (
       <BallGameWrapper>
         <h1>{this.props.title}</h1>
-        {this.state.nums.map(num => (
-          <Ball num={num} />
+        {this.state.nums.map((num, index) => (
+          <Ball key={index} num={num} />
         ))}
-        <Button>Click</Button>
+        <Button onClick={this.handleClick}>Generate</Button>
       </BallGameWrapper>
     );
   }
 }
 const BallGameWrapper = styled.div`
+  h1 {
+    display: block;
+    position: absolute;
+    bottom: 200px;
+  }
   background: #fefe;
-  width: 65%;
+  width: 95%;
   margin: 0 auto;
   border-radius: 2em;
-  padding: 4em;
+  padding: 10em;
   display: flex;
   justify-content: center;
+  position: relative;
+  button {
+    position: absolute;
+    top: 200px;
+  }
 `;
