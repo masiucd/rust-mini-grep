df3fa0c0f27b070e78ec45f91079852af9078692
marcell <ciszekmarcell@gmail.com>
2019-05-30 10:50:37 +0200

    redux

--- customers_app_redux/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import deleteContact from './deleteReducer';
-import fetchContacts from './fetchCustomer';
-
-export default combineReducers({
-  contacts: fetchContacts,
-  deleteContact,
-});
