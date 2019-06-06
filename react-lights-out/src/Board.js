e1cca87ec44f845f72da33a5535dfdb96dfc4e80
marcell <ciszekmarcell@gmail.com>
2019-06-06 15:28:06 +0200

    flipping game

---
+++ react-lights-out/src/Board.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import Cell from './Cell';
+import './Board.css';
+
+class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartsOn: 0.25,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasWon: false,
+      board: this.createBoard(),
+    };
+  }
+
+  createBoard() {
+    const board = [];
+
+    for (let y = 0; y < this.props.nrows; y++) {
+      const row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartsOn);
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  flipCellsAround(coord) {
+    const { ncols, nrows } = this.props;
+    const { board } = this.state;
+    const [y, x] = coord.split('-').map(Number);
+
+    function flipCell(y, x) {
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+    }
+
+    flipCell(y, x); // Flip initial cell
+    flipCell(y, x - 1); // flip left
+    flipCell(y, x + 1); // flip right
+    flipCell(y - 1, x); // flip below
+    flipCell(y + 1, x); // flip above
+
+    // win when every cell is turned off
+    // TODO: determine is the game has been won
+    const hasWon = board.every(row => row.every(cell => !cell));
+
+    this.setState({ board, hasWon });
+  }
+
+  makeTable() {
+    const tblBoard = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      const row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        const coord = `${y}-${x}`;
+        row.push(
+          <Cell
+            key={coord}
+            isLit={this.state.board[y][x]}
+            flipCellsAroundMe={() => this.flipCellsAround(coord)}
+          />
+        );
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>);
+    }
+    return (
+      <table className="Board">
+        <tbody>{tblBoard}</tbody>
+      </table>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.hasWon ? (
+          <div className="winner">
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WIN!</span>
+          </div>
+        ) : (
+          <div>
+            <div className="Board-title">
+              <div className="neon-orange">Lights</div>
+              <div className="neon-blue">Out</div>
+            </div>
+            {this.makeTable()}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Board;
